631c62765b6ae1da48ae98f4733d597b2b92048f
Miranda Morris <94880693+Juniperistic@users.noreply.github.com>
2023-07-15 19:42:36 -0400

    Upload of Combo Box State Objects

---
+++ ch15ex3/ch15ex3/ch15ex3/frmComboBoxes.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace ch15ex3
+{
+    partial class cmbStates
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alaska",
+            "Alabama",
+            "Arizona",
+            "New York",
+            "New Jersey",
+            "Pennsylvania",
+            "Washington",
+            "Michigan",
+            "Wisconsin",
+            "Indiana",
+            "Texas",
+            "Florida",
+            "Ohio",
+            "Nevada",
+            "Delaware"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(321, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmbStates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 267);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "cmbStates";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.cmbStates_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ch15ex3/ch15ex3/ch15ex3/frmComboBoxes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*************************************************************************/
+/* Program Name:     Ch15Ex3.cs                                          */
+/* Date:             05/01/2022                                    	     */
+/* Programmer:       Miranda Morris                                 	 */
+/* Class:            CSCI 234                               		     */
+/*                                                                       */
+/* Program Description: The purpose of this program is to create 15      */
+/* state objects within a combobox and the selected state gets removed.  */
+/*                                                                       */
+/* Input: Select a state from the comboBox								 */
+/*                                                                       */
+/* Output: The selected state gets removed from the ComboBox			 */
+/*                                                                       */
+/* Givens: The names of 15 states          								 */
+/*                                                                       */
+/* Testing Data: Screenshots included                                    */
+/*                                                                       */
+/* List the Testing Input Data:  Screenshots included                    */
+/* I opened the program selected alabama and Washington which removed    */
+/* them from the combobox                                                */
+/*                                                                       */
+/*************************************************************************/
+
+namespace ch15ex3
+{
+    public partial class cmbStates : Form
+    {
+        public cmbStates()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+        }
+
+        private void cmbStates_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
